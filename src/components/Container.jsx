6edae7a4d2fd2fbe6c714a1aa09defe76ed0f23a
AJOY SAHA <ajoy.code.dreamer@gmail.com>
2025-06-20 16:37:31 +0600

    Initial commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,8 @@
+import React, { Children } from "react";
+
+const Container = ( Children ) => {
+  return 
+  <container>{Children}</container>;
+};
+
+export default Container;
---
+++ src/pages/CheckOutPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckOutPage = () => {
+  return <div>Checkout Page</div>;
+};
+
+export default CheckOutPage;
